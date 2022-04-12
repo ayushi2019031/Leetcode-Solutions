2f0b63b247c9144b92abad9a060394907e5d2236
Ayushi Jain <73245895+ayushi2019031@users.noreply.github.com>
2022-04-12 10:58:25 +0530

    Time: 6 ms (17.45%), Space: 66.1 MB (5.45%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -1,14 +1,24 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        int left_max_val = values[0]; 
-        int n = values.length;
-        int ans = 0; 
+        int n = values.length; 
+        int[] arr1 = new int[n];
+        arr1[0] = values[0];
         for (int i = 1; i < n; i++){
-            ans = Math.max(ans, left_max_val + values[i] - i);
-            left_max_val = Math.max(left_max_val, values[i] + i);
-            
+            arr1[i] = Math.max(arr1[i - 1], values[i] + i);
         }
-        return ans;
         
+        int[] arr2 = new int[n];
+        arr2[n - 1] = values[n - 1] - (n - 1);
+        
+        
+        for (int i = n - 2; i >= 0; i--){
+         arr2[i] = Math.max(values[i] - i, arr2[i + 1]);   
+        }
+        int max = Integer.MIN_VALUE; 
+        for (int i = 0; i < n - 1; i++){
+            max = Math.max(max, arr1[i] + arr2[i + 1]);
+        }
+        
+        return max; 
     }
 }
