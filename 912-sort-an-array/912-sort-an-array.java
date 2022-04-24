aa2fe675c4be0e22877319e5d3507aa925384e0e
Ayushi Jain <73245895+ayushi2019031@users.noreply.github.com>
2022-04-24 12:04:59 +0530

    Time: 10 ms (88.65%), Space: 67.3 MB (56.44%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n = nums.length; 
+        int[] arr = new int[n]; 
+        Arrays.sort(nums);
+        return nums; 
+    }
+}
